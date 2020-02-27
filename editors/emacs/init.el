018a80272d7a86c352209f03d7ff7ddb6b766b4c
Ali Ghahraei Figueroa <aligf94@gmail.com@>
2020-02-27 01:15:38 -0600

    Remove extra spaces

--- editors/emacs/init.el
+++ editors/emacs/init.el
@@ -150,7 +150,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       python              ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
@@ -172,7 +172,7 @@
 
        :app
        ;;calendar
-       irc               ; how neckbeards socialize
+       irc                 ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
