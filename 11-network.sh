6337df3f692eb3fd25a80bd7023157d28a7dbd4a
Ali Ghahraei <aligf94@gmail.com>
2021-03-23 12:48:23 -0600

    Autoconnect to network profile

--- 11-network.sh
+++ 11-network.sh
@@ -8,6 +8,5 @@ AddPackage dialog 								# For wifi-menu
 
 # Files
 CopyFile /etc/group								# To add dhcpcd group
-
-## After connecting to wifi
-CopyFile /etc/resolv.conf '' systemd-resolve systemd-resolve
+CopyFile /etc/resolv.conf '' systemd-resolve systemd-resolve			# After connecting to wifi
+CreateLink /etc/systemd/system/sys-subsystem-net-devices-wlo1.device.wants/netctl-auto@wlo1.service /usr/lib/systemd/system/netctl-auto@.service	# Auto-connect
