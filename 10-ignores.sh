eefd50c2d8c7edeb3a377074d8e0845ae258273d
Ali Ghahraei <aligf94@gmail.com>
2021-03-23 04:39:14 -0600

    Fix comment alignment

--- 10-ignores.sh
+++ 10-ignores.sh
@@ -1,7 +1,7 @@
 # Boot
-IgnorePath '/boot/*.img'					# OS images
-IgnorePath '/boot/System\ Volume\ Information'			# M$ stuff for restoration
-IgnorePath '/boot/vmlinuz-linux'				# Kernel executable
+IgnorePath '/boot/*.img'							# OS images
+IgnorePath '/boot/System\ Volume\ Information'					# M$ stuff for restoration
+IgnorePath '/boot/vmlinuz-linux'						# Kernel executable
 
 ## /boot/EFI
 IgnorePath '/boot/EFI/Boot/*'
@@ -18,14 +18,14 @@ IgnorePath '/boot/EFI/refind/vars/*'
 
 
 # /etc
-IgnorePath '/etc/*-'						# Automatic backups for important files
+IgnorePath '/etc/*-'								# Automatic backups for important files
 IgnorePath '/etc/*.cache'
 IgnorePath '/etc/*.lock'
-IgnorePath '/etc/adjtime'					# Bookkeeping file used by hwclock
+IgnorePath '/etc/adjtime'							# Bookkeeping file used by hwclock
 IgnorePath '/etc/ca-certificates/*'
-IgnorePath '/etc/gshadow'					# Group passwords
+IgnorePath '/etc/gshadow'							# Group passwords
 IgnorePath '/etc/hostname'
-IgnorePath '/etc/machine-id'					# Random identifier generated at install/boot time
+IgnorePath '/etc/machine-id'							# Random identifier generated at install/boot time
 IgnorePath '/etc/mkinitcpio.d/*'
 IgnorePath '/etc/os-release'
 IgnorePath '/etc/pacman.d/gnupg/*'
@@ -43,16 +43,16 @@ IgnorePath '/lost+found/*'
 
 
 # /usr
-IgnorePath '/usr/lib/locale/locale-archive'			# Selected locales in binary format for speed
-IgnorePath '/usr/lib/modules/*'					# Kernel modules
-IgnorePath '/usr/lib/udev/hwdb.bin'				# Udev db to apply rules to devices
-IgnorePath '/usr/share/info/dir'				# Info pages index
+IgnorePath '/usr/lib/locale/locale-archive'					# Selected locales in binary format for speed
+IgnorePath '/usr/lib/modules/*'							# Kernel modules
+IgnorePath '/usr/lib/udev/hwdb.bin'						# Udev db to apply rules to devices
+IgnorePath '/usr/share/info/dir'						# Info pages index
 
 
 # /var/lib
 IgnorePath '/var/lib/dbus/machine-id'
-IgnorePath '/var/lib/pacman/local/*'				# Pacman's local DB
-IgnorePath '/var/lib/pacman/sync/*.db'				# Pacman's repository DBs
+IgnorePath '/var/lib/pacman/local/*'						# Pacman's local DB
+IgnorePath '/var/lib/pacman/sync/*.db'						# Pacman's repository DBs
 
 # Systemd
 IgnorePath '/var/lib/machines/*'
