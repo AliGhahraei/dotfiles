eb85276ee68212b04ca5662b93340ce0aa3c2f29
Ali Ghahraei <aligf94@gmail.com>
2021-03-23 13:34:52 -0600

    Add additional terminal packages

--- 41-terminal.sh
+++ 41-terminal.sh
@@ -1,3 +1,7 @@
-#Packages
+# Packages
 AddPackage termite								# Terminal emulator
+
+## Shell
 AddPackage fzf									# Command-line fuzzy finder
+AddPackage s-nail 								# Environment for sending and receiving mail
+AddPackage thefuck 								# Magnificent app which corrects your previous console command
