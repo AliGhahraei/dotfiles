ecfe713e58ca685afb348cccd5f7851d9cc1987f
Ali Ghahraei <aligf94@gmail.com>
2021-03-23 13:33:39 -0600

    Add additional gui packages

--- 70-gui.sh
+++ 70-gui.sh
@@ -1,8 +1,4 @@
 # Packages
-## Desktop notifications
-AddPackage libnotify								# Library for notifications, needs server
-AddPackage dunst								# Notification daemon/server
-
 ## Xorg
 AddPackage xorg-server								# Enough to run Xorg
 AddPackage xorg-xinit								# Enables startx to start Xorg
@@ -12,6 +8,14 @@ AddPackage xmonad
 AddPackage xmonad-contrib							# Xmonad add-ons used in my config
 AddPackage dmenu								# Command runner
 AddPackage xmobar 								# Minimalistic Text Based Status Bar
+
+## Desktop notifications
+AddPackage libnotify								# Library for notifications, needs server
+AddPackage dunst								# Notification daemon/server
+
+## Misc
+AddPackage feh									# Background image setter & image viewer
+AddPackage picom 								# X compositor for sweet transparency
 
 
 
