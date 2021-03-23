689a9e8e9919591e45de4672b8279fec09d9f6f0
Ali Ghahraei <aligf94@gmail.com>
2021-03-23 12:20:05 -0600

    Add touchpad config

--- 70-gui.sh
+++ 70-gui.sh
@@ -9,6 +9,11 @@ AddPackage xorg-xinit								# Enables startx to start Xorg
 
 # WM
 AddPackage xmonad
-AddPackage xmonad-contrib							# Xmoand add-ons used in my config
+AddPackage xmonad-contrib							# Xmonad add-ons used in my config
 AddPackage dmenu								# Command runner
 AddPackage xmobar 								# Minimalistic Text Based Status Bar
+
+
+
+# Files
+CopyFile /etc/X11/xorg.conf.d/30-touchpad.conf 755
