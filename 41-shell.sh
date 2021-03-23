86d5a4ebfc6af500e86e9b8e491cb4cbaa9c834c
Ali Ghahraei <aligf94@gmail.com>
2021-03-23 13:37:13 -0600

    Split shell packages from emulator

--- 41-shell.sh
+++ 41-shell.sh
@@ -1,7 +1,4 @@
 # Packages
-AddPackage termite								# Terminal emulator
-
-## Shell
 AddPackage fzf									# Command-line fuzzy finder
 AddPackage s-nail 								# Environment for sending and receiving mail
 AddPackage thefuck 								# Magnificent app which corrects your previous console command
--- 90-misc.sh
+++ 90-misc.sh
@@ -1,1 +1,3 @@
+# Packages
 AddPackage firefox-developer-edition 						# Developer Edition of the popular Firefox web browser
+AddPackage termite								# Terminal emulator
