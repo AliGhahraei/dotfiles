f4b83f6d1878c75d79847e94e0c152e74ae24ede
Ali Ghahraei <aligf94@gmail.com>
2021-03-23 20:16:53 -0600

    Add tree

--- 50-sys-admin.sh
+++ 50-sys-admin.sh
@@ -1,2 +1,3 @@
-AddPackage pacutils								# Aconfmgr dependency
 AddPackage --foreign paru							# "Feature packed AUR helper" :)
+AddPackage pacutils								# Aconfmgr dependency
+AddPackage tree 								# A directory listing program displaying a depth indented list of files
