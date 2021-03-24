0e2381666ec7c04fc92d381f38597368ecaeeb19
Ali Ghahraei <aligf94@gmail.com>
2021-03-23 20:04:43 -0600

    Configure bluetooth

--- 72-bluetooth.sh
+++ 72-bluetooth.sh
@@ -1,3 +1,14 @@
 # Packages
 AddPackage bluez-utils 								# Provides bluetoothctl command
 AddPackage pulseaudio-bluetooth							# Bluetooth support for PulseAudio
+
+
+
+# Files
+## Enable bluetooth services
+CreateLink /etc/systemd/system/bluetooth.target.wants/bluetooth.service /usr/lib/systemd/system/bluetooth.service
+CreateLink /etc/systemd/system/dbus-org.bluez.service /usr/lib/systemd/system/bluetooth.service
+
+## Bluetooth config
+CopyFile /etc/bluetooth/main.conf
+CopyFile /etc/pulse/default.pa
