7d71dd79279f7d5961017dd32110588e0a58480e
Ali Ghahraei <aligf94@gmail.com>
2021-03-25 19:25:26 -0600

    Further configure Paru

--- 20-sys-admin.sh
+++ 20-sys-admin.sh
@@ -1,5 +1,13 @@
-# Package
+# Packages
 AddPackage --foreign paru # "Feature packed AUR helper" :)
 AddPackage htop # Interactive process viewer
 AddPackage pacutils # Aconfmgr dependency
 AddPackage tree # Visualize dirs as tree
+
+## Paru optional deps
+AddPackage bat # For colored pkgbuild printing
+
+
+
+# Files
+CopyFile /etc/paru.conf
