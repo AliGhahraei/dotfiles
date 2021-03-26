3ef9bb8a74b60dc5bca7df957e652609fe9c1314
Ali Ghahraei <aligf94@gmail.com>
2021-03-25 19:25:40 -0600

    Add ranger

--- 70-tui-apps.sh
+++ 70-tui-apps.sh
@@ -2,7 +2,11 @@
 AddPackage gnome-keyring # Stores passwords and encryption keys
 AddPackage p7zip # Command-line file archiver with high compression ratio
 AddPackage python-pipx # Install isolated global python apps
+AddPackage ranger # Simple, vim-like file manager
 AddPackage xclip # Command line interface to the X11 clipboard
 
 AddPackage xdg-utils # Provide xdg-open so apps like Emacs can find default apps
                      # (e.g. a browser)
+
+## Ranger optional deps
+AddPackage highlight # Code syntax highlighting
