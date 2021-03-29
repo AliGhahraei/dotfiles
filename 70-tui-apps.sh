db2c49e5dfaaf3ae7456ed3f30636792d6cbf607
Ali Ghahraei <aligf94@gmail.com>
2021-03-29 00:59:19 -0600

    Add taskwarrior

--- 70-tui-apps.sh
+++ 70-tui-apps.sh
@@ -4,6 +4,7 @@ AddPackage mpv # a free, open source, and cross-platform media player
 AddPackage p7zip # Command-line file archiver with high compression ratio
 AddPackage python-pipx # Install isolated global python apps
 AddPackage ranger # Simple, vim-like file manager
+AddPackage task # A command-line todo list manager
 AddPackage sxiv # Simple X Image Viewer
 AddPackage xclip # Command line interface to the X11 clipboard
 AddPackage xorg-xkill # Kill a client by its X resource
