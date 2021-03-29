3164ae720819b1d1e12c7a5218866b3e6a7eeda7
Ali Ghahraei <aligf94@gmail.com>
2021-03-29 07:34:24 -0600

    Handle dumb terminals like Emacs Tramp

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,48 +1,57 @@
-set -q PLATFORM_TYPE || set PLATFORM_TYPE (uname)
-
-
-if status --is-login
-    and test (uname) = "Linux"
-
-    bass source $HOME/.config/common_profile/base
-    source $__fish_config_dir/abbreviations.fish
-    exec startx $XDG_CONFIG_HOME/X11/xinitrc -- -keeptty
-end
-
-
-if status --is-interactive
-    thefuck --alias | source
-    function cd
-        builtin cd $argv
-        ls
+if test "$TERM" = "dumb"
+    function fish_prompt
+        echo "\$ "
     end
-end
+
+    function fish_right_prompt; end
+    function fish_greeting; end
+    function fish_title; end
+else
+    set -q PLATFORM_TYPE || set PLATFORM_TYPE (uname)
 
 
-function fish_greeting 
-    if mail -e
-        set mail_message "MAAAAAAAAAAAAAAAAAAAAIL!"
+    if status --is-login
+        and test (uname) = "Linux"
 
-        set_color red
-        echo $mail_message
-        set_color normal
+        bass source $HOME/.config/common_profile/base
+        source $__fish_config_dir/abbreviations.fish
+        exec startx $XDG_CONFIG_HOME/X11/xinitrc -- -keeptty
+    end
 
-        switch $PLATFORM_TYPE
-        case Darwin
-            osascript -e "display notification \"$mail_message\""
-        case Linux
-            notify-send -u critical $mail_message
+
+    if status --is-interactive
+        thefuck --alias | source
+        function cd
+            builtin cd $argv
+            ls
         end
-    else
-        set_color brmagenta
-        echo Welcome to the afterlife.
-        set_color normal
     end
-end
 
 
+    function fish_greeting
+        if mail -e
+            set mail_message "MAAAAAAAAAAAAAAAAAAAAIL!"
 
-# pyenv init
-if command -v pyenv 1>/dev/null 2>&1
-  pyenv init - | source
+            set_color red
+            echo $mail_message
+            set_color normal
+
+            switch $PLATFORM_TYPE
+                case Darwin
+                    osascript -e "display notification \"$mail_message\""
+                case Linux
+                    notify-send -u critical $mail_message
+            end
+        else
+            set_color brmagenta
+            echo Welcome to the afterlife.
+            set_color normal
+        end
+    end
+
+
+    # pyenv init
+    if command -v pyenv 1>/dev/null 2>&1
+        pyenv init - | source
+    end
 end
