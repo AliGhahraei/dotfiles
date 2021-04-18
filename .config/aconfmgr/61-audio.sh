546a1417fb6bcfe57da0f12d4908502d349b74af
Ali Ghahraei <aligf94@gmail.com>
2021-04-18 15:28:19 -0500

    Migrate to Pipewire-pulse

--- .config/aconfmgr/61-audio.sh
+++ .config/aconfmgr/61-audio.sh
@@ -1,8 +1,4 @@
 # Packages
 AddPackage pamixer # Like amixer, used to set volume with key shortcut
-AddPackage pulseaudio # A featureful, general-purpose sound server
-AddPackage pulseaudio-alsa # ALSA Configuration for PulseAudio
+AddPackage pipewire-pulse # PulseAudio replacement
 AddPackage pulsemixer # CLI and curses mixer for pulseaudio
-
-# Files
-CopyFile /etc/group # Pulseaudio changes
