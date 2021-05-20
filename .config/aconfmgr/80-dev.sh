1ade0e4aafca4554ce69fd5d37b7e2922a123c12
Ali Ghahraei <aligf94@gmail.com>
2021-05-20 01:30:24 -0500

    Add additional python development packages

--- .config/aconfmgr/80-dev.sh
+++ .config/aconfmgr/80-dev.sh
@@ -8,3 +8,6 @@ AddPackage neovim
 ## Python-related
 AddPackage ipython # An enhanced Interactive Python shell.
 AddPackage pyenv # Easily switch between multiple versions of Python
+AddPackage python-doit # A task management and automation tool
+AddPackage python-nox # Flexible test automation for Python
+AddPackage python-typer # Build great CLIs. Easy to code. Based on type hints
