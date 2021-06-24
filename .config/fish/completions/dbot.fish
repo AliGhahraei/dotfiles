69f1f1c6702dd4b04599953f1f67ab006e9ea566
Ali Ghahraei <aligf94@gmail.com>
2021-06-23 22:34:20 -0500

    Re-add plain text fish files suitable for VCS

---
+++ .config/fish/completions/dbot.fish
@@ -0,0 +1,1 @@
+complete --command dbot --no-files --arguments "(env _DBOT_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) dbot)" --condition "env _DBOT_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) dbot"
---
+++ .config/fish/completions/domestobot.fish
@@ -0,0 +1,1 @@
+complete --command domestobot --no-files --arguments "(env _DOMESTOBOT_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) domestobot)" --condition "env _DOMESTOBOT_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) domestobot"
