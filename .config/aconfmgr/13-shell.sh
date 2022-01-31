cb203582a22b9f69cc2b205c9643463a9804c020
Ali Ghahraei <aligf94@gmail.com>
2022-01-30 21:34:33 -0600

    Make direnv non-foreign

--- .config/aconfmgr/13-shell.sh
+++ .config/aconfmgr/13-shell.sh
@@ -6,7 +6,7 @@ AddPackage dash # Fast alternative for /bin/sh
 AddPackage fish
 
 ## "Add-ons" from shell configs
-AddPackage --foreign direnv-bin # manage per-directory environments
+AddPackage direnv # manage per-directory environments
 AddPackage fzf # Command-line fuzzy finder
 AddPackage s-nail # Environment for sending and receiving mail
 AddPackage thefuck # Magnificent app which corrects your previous command
