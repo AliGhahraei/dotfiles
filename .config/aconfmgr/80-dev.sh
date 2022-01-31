19efa285e4ad6f3f8a478cf845daf08a10a7d831
Ali Ghahraei <aligf94@gmail.com>
2022-01-31 01:52:45 -0600

    Add flit

--- .config/aconfmgr/80-dev.sh
+++ .config/aconfmgr/80-dev.sh
@@ -9,5 +9,6 @@ AddPackage neovim
 AddPackage ipython # An enhanced Interactive Python shell.
 AddPackage pyenv # Easily switch between multiple versions of Python
 AddPackage python-doit # A task management and automation tool
+AddPackage python-flit # Simplified packaging of Python modules
 AddPackage python-nox # Flexible test automation for Python
 AddPackage python-typer # Build great CLIs. Easy to code. Based on type hints
