a6a85d781322a36f4c11165d29b4135c2319a1c0
Ali Ghahraei <aligf94@gmail.com>
2022-02-07 12:58:14 -0600

    Add Pycharm

--- .config/aconfmgr/80-dev.sh
+++ .config/aconfmgr/80-dev.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 # Packages
 AddPackage git
 AddPackage neovim
+AddPackage pycharm-community-edition # Python IDE for Professional Developers
 
 ## Hugo-related
 AddPackage hugo # Fast and Flexible Static Site Generator in Go
@@ -16,3 +17,9 @@ AddPackage python-doit # A task management and automation tool
 AddPackage python-flit # Simplified packaging of Python modules
 AddPackage python-nox # Flexible test automation for Python
 AddPackage python-typer # Build great CLIs. Easy to code. Based on type hints
+
+
+# Files
+## Pycharm
+CreateLink /usr/lib/jvm/default java-11-openjdk
+CreateLink /usr/lib/jvm/default-runtime java-11-openjdk
