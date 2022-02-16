352f846f215caffa9dd04c5a424b886d5c559889
Ali Ghahraei <aligf94@gmail.com>
2022-02-15 23:39:00 -0600

    Remove unused package

--- .config/aconfmgr/13-shell.sh
+++ .config/aconfmgr/13-shell.sh
@@ -9,7 +9,6 @@ AddPackage fish
 AddPackage direnv # manage per-directory environments
 AddPackage fzf # Command-line fuzzy finder
 AddPackage s-nail # Environment for sending and receiving mail
-AddPackage thefuck # Magnificent app which corrects your previous command
 
 
 
