1d8520477fcabfdeefc427220ac535cd454a131c
Ali Ghahraei <aligf94@gmail.com>
2022-03-07 03:33:43 -0600

    Remove unused domestobot completion
    I only use domestobot as a library, not as an app.

--- .config/fish/completions/domestobot.fish
+++
@@ -1,1 +0,0 @@
-complete --command domestobot --no-files --arguments "(env _DOMESTOBOT_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) domestobot)" --condition "env _DOMESTOBOT_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) domestobot"
