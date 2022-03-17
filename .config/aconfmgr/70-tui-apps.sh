06c6cada5fdc7759212af51258b11ea265e86892
Ali Ghahraei <aligf94@gmail.com>
2022-03-16 23:58:24 -0600

    Remove taskwarrior
    Nowadays I only use org-mode, so I don't need it anymore.

--- .config/aconfmgr/70-tui-apps.sh
+++ .config/aconfmgr/70-tui-apps.sh
@@ -7,7 +7,6 @@ AddPackage mpv # a free, open source, and cross-platform media player
 AddPackage nnn # The fastest terminal file manager ever written.
 AddPackage p7zip # Command-line file archiver with high compression ratio
 AddPackage python-pipx # Install isolated global python apps
-AddPackage task # A command-line todo list manager
 AddPackage feh # Fast and light imlib2-based image viewer
 AddPackage xclip # Command line interface to the X11 clipboard
 AddPackage xorg-xkill # Kill a client by its X resource
