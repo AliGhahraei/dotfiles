5484ed43340baaf630c71720fb42a00c0d37ef18
Ali Ghahraei <aligf94@gmail.com>
2022-04-16 17:21:50 -0500

    Add sysadmin support for multiple platforms

--- .config/aconfmgr/20-sys-admin.sh
+++ .config/aconfmgr/20-sys-admin.sh
@@ -4,12 +4,15 @@ set -euo pipefail
 # Packages
 AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
 AddPackage --foreign paru-bin # "Feature packed AUR helper" :)
-AddPackage borg # Deduplicating backup program with compression and encryption
 AddPackage gptfdisk # For cgdisk
 AddPackage htop # Interactive process viewer
 AddPackage ncdu # Disk usage analyzer with an ncurses interface
 AddPackage restic # Fast, secure, efficient backup program
 AddPackage tree # Visualize dirs as tree
+
+if is_main_platform; then
+    AddPackage borg # Deduplicating backup program with compression and encryption
+fi
 
 ## Paru optional deps
 AddPackage bat # For colored pkgbuild printing
