f9d45ee7e5ba565b6ee88ddd5bba85a41c235805
Ali Ghahraei <aligf94@gmail.com>
2022-04-17 01:30:25 -0500

    Add NPM

--- .config/aconfmgr/80-dev.sh
+++ .config/aconfmgr/80-dev.sh
@@ -14,6 +14,9 @@ else
     AddPackage docker # Pack, ship and run any application as a lightweight container
     AddPackage docker-compose # Fast, isolated development environments using Docker
     AddPackage --foreign postman-bin # Build, test, and document your APIs faster
+
+    ## JS-related
+    AddPackage npm # A package manager for javascript
 fi
 
 ## Python-related
