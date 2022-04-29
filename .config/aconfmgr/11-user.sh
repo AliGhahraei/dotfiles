331ef58564e45e2cbdca1e9bf48791151e6174d1
Ali Ghahraei <aligf94@gmail.com>
2022-04-28 21:42:39 -0500

    Disable autologin as it's no longer needed

--- .config/aconfmgr/11-user.sh
+++ .config/aconfmgr/11-user.sh
@@ -2,11 +2,6 @@
 set -euo pipefail
 
 # Files
-## Setup Autologin
-CreateLink /etc/systemd/system/getty.target.wants/getty@tty1.service \
-    /usr/lib/systemd/system/getty@.service
-CopyFile /etc/systemd/system/getty@tty1.service.d/override.conf
-
 ## Make apps comply with XDG
 MakeUserDir "$XDG_STATE_HOME/less"
 MakeUserDir "$XDG_STATE_HOME/bash"
