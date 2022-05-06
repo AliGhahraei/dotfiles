91e2c3530effa6a366b48cbbce7f5da16e7d7fe4
Ali Ghahraei <aligf94@gmail.com>
2022-05-06 03:44:43 -0500

    Go back to bitwarden (keepassxc's extension sucks)

--- .config/aconfmgr/82-keyring.sh
+++ .config/aconfmgr/82-keyring.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-AddPackage keepassxc # Cross-platform community-driven port of Keepass password manager
-AddPackage inotify-tools # For Keepassxc dbs synchronization
+AddPackage rbw # Unofficial Bitwarden CLI
