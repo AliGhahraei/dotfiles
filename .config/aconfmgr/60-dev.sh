c38a73ea0d859e6aa9be71afef7db0f946c49e47
Ali Ghahraei <aligf94@gmail.com>
2022-06-03 15:36:22 -0500

    Add new dev packages

--- .config/aconfmgr/60-dev.sh
+++ .config/aconfmgr/60-dev.sh
@@ -12,12 +12,20 @@ else
     AddPackage code # The Open Source build of Visual Studio Code (vscode) editor
     AddPackage docker # Pack, ship and run any application as a lightweight container
     AddPackage docker-compose # Fast, isolated development environments using Docker
+    AddPackage dotnet-sdk # The .NET Core SDK
+    AddPackage --foreign dotnet-sdk-5.0-bin # The .NET Core SDK version 5.0 (binary) - End of life
     AddPackage --foreign mssql-server # Microsoft SQL Server for Linux
     AddPackage --foreign mssql-tools # Microsoft® SQL Server® Tools for Linux
     AddPackage --foreign postman-bin # Build, test, and document your APIs faster
+    AddPackage --foreign rider # A cross-platform .NET IDE by JetBrains.
 
+    ## Kafka-related
+    AddPackage jdk8-openjdk # OpenJDK Java 8 development kit
+    AddPackage librdkafka # The Apache Kafka C/C++ library
+    AddPackage ts-node # TypeScript execution and REPL for node.js, with source map support
     ## JS-related
     AddPackage npm # A package manager for javascript
+    AddPackage pm2 # Production process manager for Node.JS applications with a built-in load balancer
     ## SQL Server dependencies
     AddPackage --foreign libldap24 # Lightweight Directory Access Protocol (LDAP) client libraries
 fi
