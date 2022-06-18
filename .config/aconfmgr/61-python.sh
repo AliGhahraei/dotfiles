2edc61605a9be1fdfdd8dd622db2c634b84da9c0
Ali Ghahraei <aligf94@gmail.com>
2022-06-17 19:09:31 -0500

    Add poetry

--- .config/aconfmgr/61-python.sh
+++ .config/aconfmgr/61-python.sh
@@ -8,6 +8,7 @@ AddPackage pyenv # Easily switch between multiple versions of Python
 AddPackage python-doit # A task management and automation tool
 AddPackage python-flit # Simplified packaging of Python modules
 AddPackage python-nox # Flexible test automation for Python
+AddPackage python-poetry # Python dependency management and packaging made easy
 AddPackage python-typer # Build great CLIs. Easy to code. Based on type hints
 
 
