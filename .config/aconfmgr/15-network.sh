4c1964144f335d88e9246954229a3399e7151e01
Ali Ghahraei <aligf94@gmail.com>
2022-06-17 19:08:32 -0500

    Revert "Switch to NetworkManager"
    This reverts commit 6c737a253869a9fbf892414c7788b6a4dffcbee4.
    NetworkManager was cool, but iwd suddenly stopped acting out on me.

--- .config/aconfmgr/15-network.sh
+++ .config/aconfmgr/15-network.sh
@@ -2,15 +2,28 @@
 set -euo pipefail
 
 # Packages
-AddPackage networkmanager # Network connection manager and user applications
-AddPackage nm-connection-editor # NetworkManager GUI connection editor and widgets
-AddPackage network-manager-applet # Applet for managing network connections
+AddPackage iwd # Network manager
+
+if ! is_main_platform; then
+    AddPackage openfortivpn # An open implementation of Fortinet's proprietary PPP+SSL VPN solution
+    AddPackage systemd-resolvconf # So OpenFortiVPN can set name servers and search domains
+
+    # Files
+    CopyFile /etc/openfortivpn/config.gpg
+    CopyFile /etc/systemd/system/vpn.service
+fi
+
 
 # Files
-## Network manager
-CreateLink /etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service \
-    /usr/lib/systemd/system/NetworkManager-dispatcher.service
-CreateLink /etc/systemd/system/multi-user.target.wants/NetworkManager.service \
-    /usr/lib/systemd/system/NetworkManager.service
-CreateLink /etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service \
-    /usr/lib/systemd/system/NetworkManager-wait-online.service
+CopyFile /etc/iwd/main.conf
+CreateLink /etc/systemd/system/multi-user.target.wants/iwd.service \
+    /usr/lib/systemd/system/iwd.service
+
+## systemd-resolved
+CreateLink /etc/systemd/system/multi-user.target.wants/systemd-resolved.service \
+    /usr/lib/systemd/system/systemd-resolved.service
+### Enable DNS
+CreateLink /etc/systemd/system/dbus-org.freedesktop.resolve1.service \
+    /usr/lib/systemd/system/systemd-resolved.service
+### Provide DNS for services that read resolv.conf directly like GnuPG
+CreateLink /etc/resolv.conf /run/systemd/resolve/stub-resolv.conf
