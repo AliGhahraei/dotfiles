bfe9e00452d364f8da9b7511947a72707e1f20c5
Ali Ghahraei <aligf94@gmail.com>
2022-09-03 16:12:12 -0500

    Switch fonts package

--- .config/aconfmgr/17-fonts.sh
+++ .config/aconfmgr/17-fonts.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 # Packages
 AddPackage ttf-fira-code # Monospaced font with programming ligatures
-AddPackage ttf-nerd-fonts-symbols # High number of extra glyphs from popular 'iconic fonts' (2048-em)
+AddPackage ttf-nerd-fonts-symbols-2048-em # High number of extra glyphs from popular 'iconic fonts' (2048-em)
 AddPackage noto-fonts-emoji # Google Noto emoji fonts
