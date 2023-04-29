7f65be91c9a09a6539f1b28d8f852ee969b8140d
Ali Ghahraei <aligf94@gmail.com>
2023-04-29 12:33:31 -0600

    Install spotify client everywhere

--- .config/aconfmgr/80-gui-apps.sh
+++ .config/aconfmgr/80-gui-apps.sh
@@ -6,12 +6,12 @@ AddPackage bitwarden # A secure and free password manager for all of your device
 AddPackage chromium # A web browser built for speed, simplicity, and security
 AddPackage firefox # Standalone web browser from mozilla.org
 AddPackage kitty # A modern, hackable, featureful, OpenGL-based terminal
+AddPackage spotify-launcher # Client for spotify's apt repository in Rust for Arch Linux
 AddPackage udiskie # Removable disk automounter using udisks
 AddPackage --foreign activitywatch-bin # Log what you do on your computer.
 
 if is_main_platform; then
     AddPackage qbittorrent # An advanced BitTorrent client programmed in C++
-    AddPackage spotify-launcher # Client for spotify's apt repository in Rust for Arch Linux
     AddPackage steam # Valve's digital software delivery system
     AddPackage wine # A compatibility layer for running Windows programs
     AddPackage wine-mono # Wine's built-in replacement for Microsoft's .NET Framework
