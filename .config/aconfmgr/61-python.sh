0fb148c46400aaa9994ef61b944b768003ff37e5
Ali Ghahraei <aligf94@gmail.com>
2023-04-29 10:32:52 -0600

    Remove pycharm (I hope I won't need it now)

--- .config/aconfmgr/61-python.sh
+++ .config/aconfmgr/61-python.sh
@@ -3,13 +3,6 @@ set -euo pipefail
 
 # Packages
 AddPackage ipython # An enhanced Interactive Python shell.
-AddPackage pycharm-community-edition # Python IDE for Professional Developers
 AddPackage pyenv # Easily switch between multiple versions of Python
 AddPackage python-flit # Simplified packaging of Python modules
 AddPackage python-typer # Build great CLIs. Easy to code. Based on type hints
-
-
-# Files
-## Pycharm
-CreateLink /usr/lib/jvm/default java-11-openjdk
-CreateLink /usr/lib/jvm/default-runtime java-11-openjdk
