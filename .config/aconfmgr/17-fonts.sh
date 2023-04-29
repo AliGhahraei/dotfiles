8b471a2743a2ae72f38be8248aac037bd5231ba4
Ali Ghahraei <aligf94@gmail.com>
2023-04-29 10:32:52 -0600

    Add asian fonts

--- .config/aconfmgr/17-fonts.sh
+++ .config/aconfmgr/17-fonts.sh
@@ -5,3 +5,4 @@ set -euo pipefail
 AddPackage ttf-fira-code # Monospaced font with programming ligatures
 AddPackage ttf-nerd-fonts-symbols-2048-em # High number of extra glyphs from popular 'iconic fonts' (2048-em)
 AddPackage noto-fonts-emoji # Google Noto emoji fonts
+AddPackage noto-fonts-cjk # Google Noto CJK fonts (asian languages)
