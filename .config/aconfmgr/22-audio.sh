2a5b39fd068fc167dc4ecc7823c94cb44a5547b1
Ali Ghahraei <aligf94@gmail.com>
2023-05-30 21:24:11 -0600

    Remove lingering wine references

--- .config/aconfmgr/22-audio.sh
+++ .config/aconfmgr/22-audio.sh
@@ -6,12 +6,6 @@ AddPackage pamixer # Like amixer, used to set volume with key shortcut
 AddPackage pipewire-pulse # PulseAudio replacement
 AddPackage pulsemixer # CLI and curses mixer for pulseaudio
 
-if is_main_platform; then
-    ## Wine game audio
-    AddPackage lib32-pipewire # Low-latency audio/video router and processor - 32-bit client library
-    AddPackage pipewire-alsa # Low-latency audio/video router and processor - ALSA configuration
-fi
-
 
 # Files
 CopyFile /etc/group # Pipewire needs rtkit group
