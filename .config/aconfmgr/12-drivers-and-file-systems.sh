1b892f1c20f66d0bcb9ad2b057c64fbba60336ff
Ali Ghahraei <aligf94@gmail.com>
2023-09-04 03:12:43 -0600

    Add exfat utils

--- .config/aconfmgr/12-drivers-and-file-systems.sh
+++ .config/aconfmgr/12-drivers-and-file-systems.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 # Packages
+AddPackage exfat-utils # Utilities for exFAT file system
 AddPackage intel-media-driver # Intel Media Driver for VAAPI — Broadwell+ iGPUs
 AddPackage libva-utils # vainfo shows if hardware acceleration is enabled
 AddPackage ntfs-3g # NTFS filesystem driver and utilities
