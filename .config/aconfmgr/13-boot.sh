6d58774aeb1c36dec09cc35a88daaf57b278f97d
Ali Ghahraei <aligf94@gmail.com>
2023-09-04 01:51:16 -0600

    Remove unused bootloaders and their config

--- .config/aconfmgr/13-boot.sh
+++ .config/aconfmgr/13-boot.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if is_main_platform; then
-    # Packages
-    AddPackage refind # Boot manager
-
-    # Files
-    CopyFile /boot/loader/entries/arch.conf 755
-    CopyFile /boot/loader/loader.conf 755
-else
-    # Packages
-    AddPackage efibootmgr # Boot manager
-
-    # Files
-    CopyFile /etc/machine-info # Kernel layout apparently used by efibootmgr
-fi
+# Files
+CopyFile /boot/loader/entries/arch.conf 755
+CopyFile /boot/loader/loader.conf 755
